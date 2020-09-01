e77fddd330e723aefb53a94d45c6b0e7df01060d
Chaitanya <chaitanyakarri963@gmail.com>
2020-09-01 16:46:27 +0530

    01-09-2020

--- ExampleMenu/app/src/main/java/com/example/chaitu/examplemenu/MainActivity.java
+++ ExampleMenu/app/src/main/java/com/example/chaitu/examplemenu/MainActivity.java
@@ -4,12 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv;
@@ -18,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = findViewById(R.id.tv);
+        registerForContextMenu(tv);
     }
 
     @Override
@@ -53,8 +60,62 @@ public class MainActivity extends AppCompatActivity {
                 });
                 builder.show();
 
-
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.mymenu,menu);
+        menu.setHeaderTitle("Select an item");
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.toast:
+                Toast.makeText(this, "I am Menu", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.name:
+                tv.setText("I am Chaitanya K");
+                break;
+            case R.id.exit:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Alert");
+                builder.setMessage("Are you sure want to exit ?");
+                builder.setIcon(R.drawable.ic_exit);
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.show();
+                break;
+            case R.id.date:
+                Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dpd = new DatePickerDialog(this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                                Toast.makeText(MainActivity.this, i+"/"+(i1+1)+"/"+i2,
+                                        Toast.LENGTH_SHORT).show();
+                                tv.setText(i2+"/"+(i1+1)+"/"+i);
+                            }
+                        },year,month,day);
+                dpd.show();
+
+        }
+        return super.onContextItemSelected(item);
     }
 }
